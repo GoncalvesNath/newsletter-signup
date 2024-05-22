a89684fb23e19166c80e2749519e65bf6262d028
Nathalia Dias <158405216+ndiasg@users.noreply.github.com>
2024-05-22 19:34:26 -0300

    projeto final

--- script.js
+++ script.js
@@ -13,8 +13,13 @@ window.addEventListener('load', changeImageOnResize);
 // Verifica a largura da tela quando a janela é redimensionada
 window.addEventListener('resize', changeImageOnResize);
 
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('subscription-form').addEventListener('submit', function(event) {
+    document.getElementById('subscription-form').addEventListener("submit", function(event) {
         event.preventDefault();  // Impede o envio do formulário
 
         const emailInput = document.getElementById('email');
@@ -23,27 +28,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // Validação simples de e-mail
         if (!validateEmail(email)) {
-            emailInput.style.border = '1px solid red';
+            emailInput.classList.add('email-error');
             errorMessage.style.display = 'block';
         } else {
-            emailInput.style.border = '1px solid #D4D4D4';
+            emailInput.classList.remove('email-error');
             errorMessage.style.display = 'none';
 
-            // Preenche o span com o e-mail do usuário
             document.getElementById('email-span').textContent = email;
-
-            // Exibe o card de sucesso
-            document.getElementById('success-subscription').style.display = 'flex';
-
-            // Oculta a section de inscrição
             document.getElementById('signup-section').style.display = 'none';
+            document.getElementById('success-subscription').style.display = 'flex';
         }
     });
 
-    function validateEmail(email) {
-        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return re.test(String(email).toLowerCase());
-    }
+    
 
     window.dismissMessage = function() {
         // Oculta o card de sucesso
